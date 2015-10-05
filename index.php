31fdd1ae70560165f8ab71a115333d389808564d
Eninfosfranco <Eninfosfranco@Eninfosfranco.mare.cl>
2015-10-05 09:58:06 -0400

    primera subida

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Parroquia Santa María Madre de la Iglesia</title>
+        
+        <!-- Bootstrap 3.0 -->
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap.css" />
+        <!-- Normalización para Web -->
+        <link rel="stylesheet" type="text/css" href="./css/normalize.css" />
+        <!-- Css Web -->
+        <link rel="stylesheet" type="text/css" href="./css/init.css" />
+        <!-- Jquery -->
+        <script type="text/javascript" src="./js/jquery.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav role="navigation">
+                <div class="navbar-fixed-top">
+                    <div class="grad_menu navbar navbar navbar-collapse">
+                        <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".bs-js-navbar-collapse">
+                                    <span class="sr-only">Toggle</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a href="" class="navbar-brand"></a>
+                            </div>
+                            <div class="navbar-collapse bs-js-navbar-collapse collapse"> 
+                                <ul class="menu nav navbar-nav navbar-right">
+                                    <li><a href="#">home</a></li>
+                                    <li><a href="#">home</a></li>
+                                    <li><a href="#">home</a></li>
+                                    <li><a href="#">home</a></li>
+                                    <li><a href="#">home</a></li>                                    
+                                </ul>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        
+        
+        <!-- Intro Section -->
+    <section id="intro" class="intro-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Scrolling Nav</h1>
+                    <p><strong>Usage Instructions:</strong> Make sure to include the <code>scrolling-nav.js</code>, <code>jquery.easing.min.js</code>, and <code>scrolling-nav.css</code> files. To make a link smooth scroll to another section on the page, give the link the <code>.page-scroll</code> class and set the link target to a corresponding ID on the page.</p>
+                    <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section id="about" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>About Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Services Section -->
+    <section id="services" class="services-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Services Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Contact Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+        
+        
+        
+        
+        
+        <script type="text/javascript" src="./js/bootstrap.js"></script>
+        
+        <script type="text/javascript">
+            $(document).ready(function() {
+
+            });
+        </script>
+    </body>
+</html>
